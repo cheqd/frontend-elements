39bd9766f973993ea23d55d50fce50964d48f395
jay-dee7 <jasdeepsingh.uppal@gmail.com>
2022-09-23 19:59:53 +0530

    feat: Converted repository to library

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Table.scss';
+
+interface IProps {
+	children?: React.ReactNode;
+	className?: string;
+	head: string[];
+}
+
+const Table = (props: IProps): JSX.Element => {
+	const { head, children, className } = props;
+	const limitLeft = head.length / 2;
+
+	return (
+		<div className={`table-responsive ${className}`}>
+			<table className="table app-table-striped table-borderless">
+				<thead>
+					<tr>
+						{head.map((value: string, index) => (
+							<th className={limitLeft <= index ? 'text-end' : 'm-4'} key={index}>
+								{value}
+							</th>
+						))}
+					</tr>
+				</thead>
+				<tbody>{children}</tbody>
+			</table>
+		</div>
+	);
+};
+
+export default Table;
---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { default as ValidatorLogo } from './ValidatorLogo/ValidatorLogo';
+export { default as Card } from './Card/Card';
+export { default as Table } from './Table/Table';
+export { default as Button } from './Button/Button';
+export { default as QRCode } from './QRCode/QRCode';
+export { default as TextInput } from './TextInput/TextInput';
+export { default as Loading } from './Loading/Loading';
