d75e40178357c6a88caf5e98408d307265dcc1eb
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2022-10-03 16:37:23 +0300

    feat: decrease package size from 3.4MB to 121KB

--- rollup.config.js
+++ rollup.config.js
@@ -1,55 +1,54 @@
-import alias from '@rollup/plugin-alias';
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import alias from "@rollup/plugin-alias";
+// import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
-import images from '@rollup/plugin-image';
-import copy from 'rollup-plugin-copy';
+import images from "@rollup/plugin-image";
+import copy from "rollup-plugin-copy";
 import path from "node:path";
+import { terser } from "rollup-plugin-terser";
 
 const pkg = require("./package.json");
 export default {
-    input: "src/index.ts",
-    output: [
+  input: "src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+    },
+    {
+      file: pkg.module,
+      format: "esm",
+    },
+  ],
+  plugins: [
+    alias({
+      entries: [{ find: "@assets", replacement: "./assets/images" }],
+    }),
+    // peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: "./tsconfig.json" }),
+    postcss({
+      autoModules: true,
+      extract: false,
+    }),
+    images(),
+    copy({
+      targets: [
         {
-            file: pkg.main,
-            format: "cjs",
-            sourcemap: true
+          src: path.resolve(__dirname, "./styles/main.scss"),
+          dest: "dist",
+          rename: "index.scss",
         },
         {
-            file: pkg.module,
-            format: "esm",
-            sourcemap: true
-        }
-    ],
-    plugins: [
-        alias({
-            entries: [
-                { find: "@assets", replacement: "./assets/images" },
-            ]
-        }),
-        peerDepsExternal(),
-        resolve(),
-        commonjs(),
-        typescript({ tsconfig: './tsconfig.json' }),
-        postcss({
-            autoModules: true,
-            extract: false,
-        }),
-        images(),
-        copy({
-            targets: [
-                {
-                    src: path.resolve(__dirname, "./styles/main.scss"),
-                    dest: "dist",
-                    rename: "index.scss"
-                },
-                {
-                    src: path.resolve(__dirname, "./styles/fonts.scss"),
-                    dest: "dist",
-                }
-            ]
-        })
-    ],
+          src: path.resolve(__dirname, "./styles/fonts.scss"),
+          dest: "dist",
+        },
+      ],
+    }),
+    terser(),
+  ],
+  external: Object.keys(pkg.peerDependencies),
 };
--- src/components/QRCode/QRCode.tsx
+++ src/components/QRCode/QRCode.tsx
@@ -1,21 +1,27 @@
-import React from 'react';
-import { QRCode as ReactQRCode } from 'react-qrcode-logo';
-import LogoDark from '../../../assets/images/cheqd-logo.png';
+import React from "react";
+import { QRCode as ReactQRCode } from "react-qrcode-logo";
+import LogoDark from "../../../assets/images/cheqd-logo.png";
 
 interface IProps {
-	content: string;
-	size?: number;
-	color?: string;
+  content: string;
+  size?: number;
+  color?: string;
 }
 
 const QRCode = (props: IProps): JSX.Element => {
-	const { content, size: sizeProps, color } = props;
-	const size = sizeProps || 80;
-	const logoSize = size / 5.0;
+  const { content, size: sizeProps, color } = props;
+  const size = sizeProps || 80;
+  const logoSize = size / 5.0;
 
-	return (
-		<ReactQRCode removeQrCodeBehindLogo value={content} logoImage={LogoDark} logoWidth={logoSize} logoHeight={logoSize} />
-	);
+  return (
+    <ReactQRCode
+      removeQrCodeBehindLogo
+      value={content}
+      logoImage={LogoDark}
+      logoWidth={logoSize}
+      logoHeight={logoSize}
+    />
+  );
 };
 
 export default QRCode;
